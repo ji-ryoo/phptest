f47a41cbdfbd0be687c002cd44aea039360b5457
ji-ryoo <tremolorj09@gmail.com>
2018-03-04 21:47:03 +0900

    first
    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$hello = "hello world";
+
+ ?>
+
+ <html>
+ <head>
+   <meta charset="utf-8">
+ </head>
+ <body>
+   <h1><?php print($hello) ?>
+     <h2>ちゃんと表示されたかな</h2>
+     
+ </body>
+ </html>
